c5503708462f4c37778f7ea7ea266a556d3e3d93
Brandon Kelton <brandon.kelton@outlook.com>
2019-08-28 18:10:37 -0600

    initial solution commit

---
+++ WirelessPrototype/WirelessPrototype/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WirelessPrototype.Models;
+
+namespace WirelessPrototype.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
